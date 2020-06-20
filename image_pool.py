15cc181aec458df9c9ea6cd2f55e62d77b9812eb
RangerUFO <ufownl@gmail.com>
2020-06-20 22:24:33 +0800

    Fix the image pool

--- image_pool.py
+++ image_pool.py
@@ -1,20 +1,18 @@
 import random
 import mxnet as mx
-import gluoncv as gcv
 
 class ImagePool:
     def __init__(self, size):
         self._size = size
         if size > 0:
             self._images = []
-        self._batchify_fn = gcv.data.batchify.Stack()
 
     def query(self, imgs):
         if self._size <= 0:
             return imgs
         ret_imgs = []
         for i in range(imgs.shape[0]):
-            img = imgs[i]
+            img = imgs[i].expand_dims(0)
             if len(self._images) < self._size:
                 self._images.append(img)
                 ret_imgs.append(img)
@@ -26,7 +24,7 @@ class ImagePool:
                     self._images[idx] = img
                 else:
                     ret_imgs.append(img)
-        return self._batchify_fn(ret_imgs)
+        return mx.nd.concat(*ret_imgs, dim=0)
 
 
 if __name__ == "__main__":
